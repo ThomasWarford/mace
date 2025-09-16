836ddcbf0a41429f5cc0f552bf3c4b7ad1d0d24c
Thomas Warford <thomasdwarford@hotmail.co.uk>
2025-09-16 21:30:51 +0000

    cleanup

--- benchmarks/run_hyper_array.sh
+++ benchmarks/run_hyper_array.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=infer_hyper
 #SBATCH --output=logs/slurm_infer_hyper_%A_%a.out
 #SBATCH --error=logs/slurm_infer_hyper_%A_%a.err
-#SBATCH --array=1
+#SBATCH --array=1-144
 #SBATCH --gpus=1
 #SBATCH --time=02:00:00
 
@@ -18,12 +18,6 @@ PARAMS=$(sed -n "${LINE}p" "$PARAM_FILE")
 
 IFS=',' read -r max_ell hidden_irreps edge_irreps <<< "$PARAMS"
 
-# # Convert enable_cueq to bool for pytest
-# if [ "$enable_cueq" -eq 1 ]; then
-#     enable_cueq_str="True"
-# else
-#     enable_cueq_str="False"
-# fi
 
 label="${edge_irreps//[$'\n\r']/}-${hidden_irreps//[$'\n\r']/}-${max_ell//[$'\n\r']/}"
 echo "${label}"
@@ -31,13 +25,6 @@ echo "${label}"
 
 # Run pytest for this parameter set
 pytest tests/test_inference_hypers.py \
-    -x \
     -v \
     -k "${label}" \
     --benchmark-save="infer_hyper_${SLURM_ARRAY_TASK_ID}"
-
-#     # Construct the nodeid string
-# NODEID="tests/test_inference_hypers.py::test_inference[${size}-${edge_irreps}-${hidden_irreps}-${num_interactions}-${max_ell}-${enable_cueq_str}-${dtype}-${correlation}-${interaction_cls}-${interaction_cls_first}]"
-
-# # Run pytest for this nodeid
-# pytest "$NODEID" --benchmark-save="infer_hyper_${SLURM_ARRAY_TASK_ID}"
